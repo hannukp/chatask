f6aecda9dd12b87c0a5fb918d025f25ca06506e5
Hannu Kankaanpää <hannu.kp@gmail.com>
2023-03-09 13:43:06 +0200

    support for temperatures

--- ask.py
+++ ask.py
@@ -16,20 +16,22 @@ def send_post_request(url: str, data: dict) -> str:
     return response.read().decode("utf-8")
 
 
-def query_chatgpt(messages):
+def query_chatgpt(messages, temperature):
     resp = send_post_request(
-        "https://api.openai.com/v1/chat/completions", {"model": "gpt-3.5-turbo", "messages": messages}
+        "https://api.openai.com/v1/chat/completions",
+        {"model": "gpt-3.5-turbo", "messages": messages, "temperature": temperature}
     )
     return json.loads(resp)["choices"][0]["message"]["content"]
 
 
 class ChatAsk:
-    def __init__(self, context=None):
+    def __init__(self, context=None, temperature=1):
+        self.temperature = temperature
         self.messages = [{"role": "system", "content": context}] if context else []
 
     def ask(self, query: str):
         self.messages.append({"role": "user", "content": query})
-        answer = query_chatgpt(self.messages)
+        answer = query_chatgpt(self.messages, self.temperature)
         # answer = "Hello!"
         self.messages.append({"role": "assistant", "content": answer})
         return answer
@@ -45,12 +47,15 @@ TEMPLATES = {
 def help_and_exit():
     print("No questions?")
     print()
+    print("Switches:")
+    print("    -t0.1  -- set temperature to 0.1 (valid range 0-2)")
+    print()
     print("Example usage:")
     print("    ask what is the meaning of life")
     print("    ask test 'const adder = (a: number, b: number) => a + b'")
     print("    ask explain 'const adder = (a: number, b: number) => a + b'")
     print("    ask test <example.py")
-    print("    ask convert to typescript <example.py")
+    print("    ask -t0 convert to typescript <example.py")
     print()
     print("Available templates:")
     for cmd, expansion in sorted(TEMPLATES.items()):
@@ -62,6 +67,13 @@ def main():
     if len(sys.argv) < 2 or "-h" in sys.argv or "--help" in sys.argv:
         help_and_exit()
 
+    temperature = 1
+    default_temperature = True
+    for a in sys.argv[1:]:
+        if a.startswith('-t'):
+            temperature = float(a[2:])
+            default_temperature = False
+
     # ignore args that look like switches
     args = [a for a in sys.argv[1:] if not a.startswith("-")]
 
@@ -69,6 +81,9 @@ def main():
     template = TEMPLATES.get(args[0])
     if template:
         args = args[1:]
+        if default_temperature:
+            # default to 0 temp for coding tasks
+            temperature = 0
 
     q = " ".join(args).strip()
     if not sys.stdin.isatty():
@@ -83,9 +98,9 @@ def main():
     if template:
         q = template.replace("*BODY*", q)
 
-    chatask = ChatAsk()
+    chatask = ChatAsk(temperature=temperature)
     print(">>>", q)
-    print()
+    print('-' * 79)
     print(chatask.ask(q))
     print()
     while True:
